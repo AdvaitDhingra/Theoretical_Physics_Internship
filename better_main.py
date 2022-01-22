34f9a28b5d385b9c42710b1b13897acde46b92f0
AdvaitDhingra <advait.dhingra1006@gmail.com>
2022-01-22 16:04:29 +0100

    Added f1 and f2 computations

--- better_main.py
+++ better_main.py
@@ -1,47 +1,40 @@
 import math
 
-def p0(k):
+p0 = [-1,3,-3,1]
+p1 = [0,-3,10,-10]
+p2 = [0,0,9,9]
 
-    return k**3-3*k**2+3*k-1
+def prod(l):
+    p = 1
+    for a in l:
+        p *= a
+    return p
 
-def p1(k):
+def diff(p,d):
+    return [a*prod([i-j for j in range(0,d)]) for i,a in enumerate(p)][d:]
 
-    return -10*k**3+10*k**2-3*k
-
-def p2(k):
-
-    return 9*k**3+9*k**2
-
-def p0d(k):
-    return 3*k**2-6*k+3
-
-def p1d(k):
-    return -30*k**2+10*k-3
-
-def p2d(k):
-    return 27*k**2+18*k
-
-
+def ev(p):
+    return lambda k : sum([a*k**i for i,a in enumerate(p)])
 
 # Computations:
 
 n = 100
 
 g0 = [0,1]
-g1a = [0,1]
-g1b = [0,1]
+g1 = [0,0]
+g2 = [0,0]
 
 for k in range(2,n):
-
-    g0.append(-sum([p*a for p,a in zip([p1(k-1),p2(k-2)],g0[-3:][::-1])])/p0(k))
-    g1b.append(-sum([p*b for p,b in zip([p1(k-1),p2(k-2)],g1b[-3:][::-1])])/p0(k))
-    g1a.append(-sum([p*a for p,a in zip([p1d(k-1),p2d(k-2)],g0[-3:][::-1])])/p0d(k))
+    g0.append(-sum([p*a for p,a in zip([ev(p1)(k-1),ev(p2)(k-2)],g0[-3:][::-1])])/ev(p0)(k))
+    g1.append(-(sum([p*a for p,a in zip([ev(p1)(k-1),ev(p2)(k-2)],g1[-3:][::-1])])+sum([g0[k]*ev(diff(p0,1))(k),g0[k-1]*ev(diff(p1,1))(k-1),g0[k-2]*ev(diff(p2,1))(k-2)]))/ev(p0)(k))
+    g2.append(-(sum([p*a for p,a in zip([ev(p1)(k-1),ev(p2)(k-2)],g2[-3:][::-1])])+sum([g1[k]*ev(diff(p0,1))(k),g1[k-1]*ev(diff(p1,1))(k-1),g1[k-2]*ev(diff(p2,1))(k-2)])+sum([g0[k]*ev(diff(p0,2))(k),g0[k-1]*ev(diff(p1,2))(k-1),g0[k-2]*ev(diff(p2,2))(k-2)])/2)/ev(p0)(k))
 
 answer1 = sum([a*(1/18)**i for i,a in enumerate(g0)])
-answer2 = math.log(1/18)*sum([a*(1/18)**i for i,a in enumerate(g0)]) + sum([b*(1/18)**i for i,b in enumerate(g1b)])
+answer2 = math.log(1/18)*sum([a*(1/18)**i for i,a in enumerate(g0)]) + sum([b*(1/18)**i for i,b in enumerate(g1)])
+answer3 = (math.log(1/18)**2)/2*sum([a*(1/18)**i for i,a in enumerate(g0)]) + math.log(1/18)*sum([b*(1/18)**i for i,b in enumerate(g1)]) + sum([c*(1/18)**i for i,c in enumerate(g2)])
 
 print(f"""
 F0 ANSWER: {answer1}
 F1 ANSWER: {answer2}
+F2 ANSWER: {answer3}
 """)
-
